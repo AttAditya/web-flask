8f46296e8f7442b806638cba1d5997ea88331440
AttAditya <attachment.aditya@gmail.com>
2024-10-06 04:58:16 +0530

    Started fixing database

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.getLogger("werkzeug").setLevel(logging.ERROR)
 app = Flask("TWTDB", static_folder="res", static_url_path="/res")
 app.secret_key = environ.get("SECRET", "default")
 
-from modules.databases import databases
+from modules.database import databases
 from modules.routes import add_routes
 
 add_routes(app, databases)
